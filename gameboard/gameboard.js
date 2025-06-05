4b13ccbcdf8c5af690d734ccc3e5e5cb0db0334e
zenith <zenithyap88@gmail.com>
2025-06-05 11:00:54 +0800

    Add placeShip method

--- gameboard/gameboard.js
+++ gameboard/gameboard.js
@@ -17,6 +17,30 @@ class Gameboard {
     get grid() {
         return this._grid;
     }
+
+    #isWithinGrid(shipLength, row, col, orientation) {
+        if (orientation === 'vertical') {
+            return row >= 0 && row + shipLength < Gameboard.ROWS && col >= 0 && col < Gameboard.COLS;
+        } else {
+            return col >= 0 && col + shipLength < Gameboard.COLS && row >= 0 && row < Gameboard.ROWS;
+        }
+    }
+
+    placeShip(ship, row, col, orientation) {
+        const shipLength = ship.length;
+
+        if (!this.#isWithinGrid(shipLength, row, col, orientation)) {
+            throw new RangeError('Ship placement is out of the grid.');
+        }
+
+        for (let i = 0; i < shipLength; i++) {
+            if (orientation === 'vertical') {
+                this._grid[row + i][col].hasShip = true;
+            } else {
+                this._grid[row][col + i].hasShip = true;
+            }
+        }
+    }
 }
 
 export default Gameboard;
--- gameboard/gameboard.test.js
+++ gameboard/gameboard.test.js
@@ -1,3 +1,4 @@
+import Ship from "../ship/ship";
 import Gameboard from "./gameboard";
 
 describe('Gameboard', () => {
@@ -14,7 +15,7 @@ describe('Gameboard', () => {
     test('gameboard should have a 10 by 10 grid', () => {
         expect(gameboard.grid.length).toBe(10);
         for (let i = 0; i < gameboard.length ; i++) {
-            expect(gameboard.grid[i].   length).toBe(10);
+            expect(gameboard.grid[i].length).toBe(10);
         }
     });
 
@@ -26,5 +27,35 @@ describe('Gameboard', () => {
                 expect(cell).toHaveProperty('isHit', false);
             }
         }
+    });
+
+    test('ship of length 2 should be placed vertically', () => {
+        const SHIP_LENGTH = 2
+        const ROW = 1;
+        const COL = 1;
+        const ship = new Ship(SHIP_LENGTH);
+        gameboard.placeShip(ship, ROW, COL, 'vertical');
+        expect(gameboard.grid[1][1].hasShip).toBe(true);
+        expect(gameboard.grid[2][1].hasShip).toBe(true);
+        expect(gameboard.grid[3][1].hasShip).toBe(false);
+    });
+
+    test('ship of length 4 should be place horizontally', () => {
+        const SHIP_LENGTH = 4;
+        const ROW = 3;
+        const COL = 4;
+        const ship = new Ship(SHIP_LENGTH);
+        gameboard.placeShip(ship, ROW, COL, 'horizontal');
+        for (let i = 0; i < SHIP_LENGTH; i++) {
+            expect(gameboard.grid[ROW][COL + i].hasShip).toBe(true);
+        }
+    });
+
+    test('ship should not be allowed to be placed outside grid', () => {
+        const SHIP_LENGTH = 1;
+        const ROW = 11;
+        const COL = 11;
+        const ship = new Ship(SHIP_LENGTH);
+        expect(() => gameboard.placeShip(ship, ROW, COL, 'horizontal')).toThrow();
     });
 });
